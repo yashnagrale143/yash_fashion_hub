1218881e1abc51ec1fe03ee8b12d534d9a24f333
yashnagrale143 <yashnagrale1434@gmail.com>
2025-08-18 21:21:27 +0530

    Delete main.jsx

--- main.jsx
+++
@@ -1,3 +0,0 @@
-import React from "react";
-import { createRoot } from 'react-dom/client';
-createRoot(document.getElementById('root')).render(<div style={{padding:20}}><h2>Yash Admin (placeholder)</h2></div>);
